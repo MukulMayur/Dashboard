80dbf1cecc41b4e59eb606fd66b80467db5f8ec0
Mukul <mukulmayur000@gmail.com>
2024-09-29 23:51:24 +0530

    Rwd

--- src/Components/Header/MessagePopover.jsx
+++ src/Components/Header/MessagePopover.jsx
@@ -5,34 +5,34 @@ function MessagePopover() {
   const [useTab, setuseTab] = useState("messages");
 
   return (
-    <div className="absolute right-0 mt-2 w-72 rounded-sm shadow-lg z-10 flex flex-col">
+    <div className="absolute right-0 mt-2 w-72 rounded-md shadow-lg z-10 flex flex-col border border-white">
       <div
-        className="w-72 md:h-[72px] lg:h-[82px] sm:h-[50px] rounded-t-sm relative flex flex-col items-center sm:p-1 md:p-2 lg:p-3 xl:p-4 bg-cover bg-center"
+        className="w-full md:h-[72px] lg:h-[82px] h-[60px] rounded-t-md relative flex flex-col items-center sm:p-1 md:p-2 lg:p-3 xl:p-4 bg-cover bg-center"
         style={{ backgroundImage: `url(${Message})` }}
       >
-        <h5 className="lg:font-semibold sm:font-sm text-slate-900">
+        <h5 className="lg:font-semibold sm:font-sm text-red-600">
           Notifications
         </h5>
-        <h6 className="lg:text-sm md:xs mt-1 text-slate-800">
+        <h6 className="lg:text-sm md:xs mt-1 text-slate-700">
           You have
           <b> 7</b> unread messages.
         </h6>
       </div>
-      <div className="flex p-2 px-3 bg-white justify-between border border-b-gray-300">
+      <div className="flex p-2 px-3 bg-[#282828] justify-between rounded-none">
         <button
-          className="lg:p-1 lg:w-[80px] lg:text-sm md:text-xs text-gray-800 rounded-sm hover:bg-indigo-500 hover:shadow-[0px_2px_10px_0px_rgba(0,0,0,0.5)] hover:text-white focus:bg-indigo-500 focus:shadow-gray-300 focus:text-white active:bg-indigo-400 active:shadow-lg active:text-white"
+          className="px-1.5 lg:p-1 lg:w-[80px] lg:text-sm md:text-xs text-gray-700 rounded-sm bg-white  hover:bg-indigo-500 hover:shadow-[0px_2px_10px_0px_rgba(0,0,0,0.5)] hover:text-white focus:bg-indigo-500 focus:shadow-gray-300 focus:text-white active:bg-indigo-400 active:shadow-lg active:text-white"
           onClick={() => setuseTab("messages")}
         >
           Messages
         </button>
         <button
-          className="lg:p-1 lg:w-[80px] lg:text-sm md:text-xs text-gray-800 rounded-sm hover:bg-indigo-500 hover:shadow-[0px_2px_10px_0px_rgba(0,0,0,0.5)] hover:text-white focus:bg-indigo-500 focus:shadow-gray-300 focus:text-white active:bg-indigo-400 active:shadow-lg active:text-white"
+          className="px-2 lg:p-1 lg:w-[80px] lg:text-sm md:text-xs text-gray-700 rounded-sm bg-white hover:bg-indigo-500 hover:shadow-[0px_2px_10px_0px_rgba(0,0,0,0.5)] hover:text-white focus:bg-indigo-500 focus:shadow-gray-300 focus:text-white active:bg-indigo-400 active:shadow-lg active:text-white"
           onClick={() => setuseTab("events")}
         >
           Events
         </button>
         <button
-          className="lg:p-1 lg:w-[80px] lg:text-sm md:text-xs text-gray-800 rounded-sm hover:bg-indigo-500 hover:shadow-[0px_2px_10px_0px_rgba(0,0,0,0.5)] hover:text-white focus:bg-indigo-500 focus:shadow-gray-300 focus:text-white active:bg-indigo-400 active:shadow-lg active:text-white"
+          className="px-2 lg:p-1 lg:w-[80px] lg:text-sm md:text-xs text-gray-700 rounded-sm bg-white hover:bg-indigo-500 hover:shadow-[0px_2px_10px_0px_rgba(0,0,0,0.5)] hover:text-white focus:bg-indigo-500 focus:shadow-gray-300 focus:text-white active:bg-indigo-400 active:shadow-lg active:text-white"
           onClick={() => setuseTab("errors")}
         >
           Errors
@@ -40,7 +40,7 @@ function MessagePopover() {
       </div>
       <div>
         {useTab && (
-          <div className="absolute p-2 bg-white  shadow-lg w-72 rounded-b-sm">
+          <div className="absolute p-2 bg-[#282828] shadow-lg w-full">
             {useTab === "messages" && (
               <div className="p-2 h-[170px] flex flex-col ">
                 <div className="flex space-x-3">
@@ -104,11 +104,6 @@ function MessagePopover() {
                   </div>
                 </div>
                 <hr />
-                <div className="text-center mt-3">
-                  <button className="bg-black text-slate-200 p-1 text-xs rounded-full px-2 active:bg-gray-700">
-                    View latest Changes
-                  </button>
-                </div>
               </div>
             )}
             {useTab === "events" && (
@@ -160,13 +155,7 @@ function MessagePopover() {
                     </div>
                   </div>
                 </div>
-
                 <hr />
-                <div className="text-center mt-3">
-                  <button className="bg-black text-slate-200 p-1 text-xs rounded-full px-2 active:bg-gray-700">
-                    View latest Changes
-                  </button>
-                </div>
               </div>
             )}
             {useTab === "errors" && (
@@ -232,11 +221,6 @@ function MessagePopover() {
                   </div>
                 </div>
                 <hr />
-                <div className="text-center mt-3">
-                  <button className="bg-black text-slate-200 p-1 text-xs rounded-full px-2 active:bg-gray-700">
-                    View latest Changes
-                  </button>
-                </div>
               </div>
             )}
           </div>
