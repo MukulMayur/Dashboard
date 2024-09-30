8ff2188d42ce478231ca5c127bd9da52fd8670c7
Mukul <mukulmayur000@gmail.com>
2024-09-30 20:55:19 +0530

    Chartjs implementation

---
+++ src/Components/Charts/BarChart.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+
+const BarChart = () => {
+  const barChartData = {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "June",
+      "July",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Revenue",
+        data: [65, 59, 80, 81, 56, 55, 40, 59, 52, 64, 78, 88],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.8)",
+          "rgba(255, 159, 64, 0.8)",
+          "rgba(255, 205, 86, 0.8)",
+          "rgba(75, 192, 192, 0.8)",
+          "rgba(54, 162, 235, 0.8)",
+          "rgba(153, 102, 255, 0.8)",
+          "rgba(201, 203, 207, 0.8)",
+          "rgba(101, 203, 107, 0.8)",
+          "rgba(201, 122, 107, 0.8)",
+          "rgba(66, 189, 187, 0.8)",
+          "rgba(201, 203, 207, 0.8)",
+          "rgba(101, 103, 11, 0.8)", // Fixed the typo here
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+          "rgb(101, 203, 107)",
+          "rgb(201, 122, 107)",
+          "rgb(66, 189, 187)",
+          "rgb(201, 203, 207)",
+          "rgb(91, 113, 67)",
+        ],
+        borderWidth: 1,
+        borderRadius: 10,
+        barThickness: 20,
+      },
+    ],
+  };
+
+  const barChartOptions = {
+    responsive: true, // Enable responsiveness
+    maintainAspectRatio: false, // Allow height adjustment
+    scales: {
+      x: {
+        grid: { display: false },
+        ticks: { color: "#FFFFFF" },
+        title: {
+          display: true,
+          text: "Months",
+          color: "#6e3cd2",
+          font: { size: 18, weight: "bold" },
+        },
+      },
+      y: {
+        grid: { display: false },
+        ticks: {
+          color: "#FFFFFF",
+          callback: function (value) {
+            return value + "M"; // Add "M" to indicate millions
+          },
+        },
+        title: {
+          display: true,
+          text: "Revenue (Million $)",
+          color: "#6e3cd2",
+          font: { size: 18, weight: "bold" },
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        labels: {
+          color: "#6e3cd2",
+          font: { size: 20, weight: "bold" },
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="flex w-full rounded-xl h-[40vh] md:h-[50vh] lg:h-[ bg-[#282828] p-4 text-white">
+      <Bar data={barChartData} options={barChartOptions} />
+    </div>
+  );
+};
+
+export default BarChart;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,49 +1,9 @@
 import React from "react";
 import TopDisplay from "../Components/Home_Components/TopDisplay";
 import Analytics from "../Components/Home_Components/Analytics";
-import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import BarChart from "../Components/Charts/BarChart";
 
 function Home() {
-  const barChartData = {
-    labels: ["A", "B", "C", "D", "E", "F", "G"],
-    datasets: [
-      {
-        label: "My Dataset",
-        data: [65, 59, 80, 81, 56, 55, 40], // Y-axis data
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(201, 203, 207, 0.2)",
-        ],
-        borderColor: [
-          "rgb(255, 99, 132)",
-          "rgb(255, 159, 64)",
-          "rgb(255, 205, 86)",
-          "rgb(75, 192, 192)",
-          "rgb(54, 162, 235)",
-          "rgb(153, 102, 255)",
-          "rgb(201, 203, 207)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const barChartOptions = {
-    scales: {
-      x: {
-        grid: {
-          offset: true,
-        },
-      },
-    },
-  };
-
   return (
     <div className="flex flex-col">
       <div className="px-3 pt-3 md:p-0 lg:p-0">
@@ -52,14 +12,49 @@ function Home() {
       <div className="flex flex-col md:flex lg:flex-row p-2 space-x-2">
         <TopDisplay />
       </div>
-      <div className="flex flex-1 px-3 gap-4 bg-[#010409]">
-        <div className="flex w-full rounded-xl h-[50vh] bg-[#282828] p-4">
-          <Bar data={barChartData} options={barChartOptions} />{" "}
-          {/* Pass data and options correctly */}
+      <div className="flex px-3 gap-2 w-full bg-[#010409] mb-4 md:flex-row lg:flex-row flex-col">
+        <div className="md:flex-[5] w-full rounded-xl md:p-2 lg:p-2 bg-[#282828]">
+          <BarChart />
+          <h2 className="p-4 px-6 text-xl text-[#914cd6] font-bold pt-0 md:pb-2 lg:pb-2 overflow-y-auto">
+            Quarterly Shipping Data
+          </h2>
+          <div className="p-4 pt-0 md:p-4 lg:p-4 h-[28vh] md:h-[35vh] lg:h-[35vh] overflow-y-auto">
+            <div className="border-[2px] border-gray-500 overflow-hidden rounded-md text-sm md:text-md lg:text-md">
+              <div className="flex text-white font-semibold bg-transparent p-2  justify-between border-b-[2px] border-gray-500">
+                <div className="flex-1 text-center">Quater</div>
+                <div className="flex-1 text-center">Shipments</div>
+                <div className="flex-1 text-center">Days</div>
+                <div className="flex-1 text-center">Costs($)</div>
+              </div>
+              <div className="bg-transparent text-white p-2 flex justify-between border-b-[2px] border-gray-500">
+                <div className="flex-1 text-center">1st Quater</div>
+                <div className="flex-1 text-center">120</div>
+                <div className="flex-1 text-center">5</div>
+                <div className="flex-1 text-center">$ 15,000</div>
+              </div>
+              <div className="bg-transparent text-white p-2 flex justify-between border-b-[2px] border-gray-500">
+                <div className="flex-1 text-center">2nd Quater</div>
+                <div className="flex-1 text-center">130</div>
+                <div className="flex-1 text-center">4</div>
+                <div className="flex-1 text-center">$ 18,000</div>
+              </div>
+              <div className="bg-transparent text-white p-2 flex justify-between border-b-[2px] border-gray-500">
+                <div className="flex-1 text-center">3rd Quater</div>
+                <div className="flex-1 text-center">150</div>
+                <div className="flex-1 text-center">3</div>
+                <div className="flex-1 text-center">$ 20,000</div>
+              </div>
+              <div className="bg-transparent text-white p-2 flex justify-between">
+                <div className="flex-1 text-center">4th Quater</div>
+                <div className="flex-1 text-center">140</div>
+                <div className="flex-1 text-center">4</div>
+                <div className="flex-1 text-center">$ 19,000</div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex w-full  rounded-xl h-[50vh] p-4 bg-[#282828]">
-          Chart1
-        </div>
+        <div className="flex-[2.5] rounded-xl p-4 bg-[#282828]">Chart1</div>
+        <div className="flex-[2.5] rounded-xl p-4 bg-[#282828]">Chart1</div>
       </div>
     </div>
   );
